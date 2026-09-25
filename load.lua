f3eab84b2050a5dd1eb9388fa6cac022e422b18c
SantxDEV <santxnobody@gmail.com>
2026-09-25 19:35:38 -0300

    fix: IgnoreNotArchivable=false, mode=full por padrao

---
+++ load.lua
@@ -0,0 +1,12 @@
+-- SaveInstance
+-- github.com/santxnobody/saveinstance
+
+local synsaveinstance = loadstring(
+    game:HttpGet("https://raw.githubusercontent.com/SantxDEV/saveinstance/main/saveinstance.luau", true),
+    "saveinstance"
+)()
+
+synsaveinstance({
+    Decompile = true,
+    ShowStatus = true,
+})
